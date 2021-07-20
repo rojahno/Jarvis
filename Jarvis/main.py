4476e46b370ed659219c8773ab8b410133e62430
rojahno <andreas_sunde97@hotmail.com>
2021-07-20 13:40:40 +0200

    Added create channel and dice functions

--- Jarvis/main.py
+++ Jarvis/main.py
@@ -2,6 +2,7 @@
 import os
 import random
 
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -26,4 +27,28 @@ async def nine_nine(ctx):
     await ctx.send(response)
 
 
-bot.run(TOKEN)
+@bot.command(name='create-channel', help="creates a new channel")
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+
+@bot.command(name="d6", help="Rolls a six sided dice")
+async def roll_d6(ctx):
+    dice = str(random.choice(range(1, 7)))
+    await ctx.send(dice)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+
+
+if __name__ == "__main__":
+    print("running")
+    bot.run(TOKEN)
